19d07364cd1a497373e52a371dc98ac32c4d1bb9
Ben Gonzales <bgonzales20001@gmail.com>
2015-03-13 06:21:52 -0700

    Update lantern_rock_install.cpp

--- lantern_rock_install/lantern_rock_install.cpp
+++ lantern_rock_install/lantern_rock_install.cpp
@@ -7,11 +7,9 @@
 #include "CommandMsiExec.h"
 #include "UiAutoConfig.h"
 #include "WinReg.h"
+#include "IasVersion.h"
 
-//Json::Value root;
 UIAuto uiauto;
-//SetupRepo setuprepo;
-//JsonConfig jsonconfig;
 
 void UiAutoInstallValidate()
 {
@@ -28,7 +26,7 @@ void UiAutoInstallValidate()
 		EnumChildWindows(errorDialog, uiauto.ClickButton, (LPARAM)TEXT("OK"));
 	}
 	else {
-		std::cout << "Window not found, window text has changed, please update text." << std::endl;
+		//std::cout << "Window not found, window text has changed, please update text." << std::endl;
 	}
 
 }
@@ -57,61 +55,170 @@ void UiAutoUninstallValidate()
 	}
 }
 
+void installValidate(UiAutoConfig uiautoconfig, const char* cmdparam)
+{
+	uiautoconfig.execute(cmdparam);
+	UiAutoInstallValidate();
+}
+
+void uninstallValidate(UiAutoConfig uiautoconfig, const char* cmdparam)
+{
+	uiautoconfig.execute(cmdparam);
+	UiAutoUninstallValidate();
+}
+
+void verifyFile(IasVersion iasversion, const wchar_t *strname)
+{
+	wchar_t fileVersion[1024];
+	wchar_t productVersion[1024];
+	memset(fileVersion, '\0', 1024);
+	memset(productVersion, '\0', 1024);
+	DWORD res = iasversion.PrintFileVersion(strname, fileVersion, productVersion);
+	if (res == 0) {
+		std::wcout << "file: " << strname << std::endl;
+		std::wcout << "\tproduct version:" << productVersion << std::endl;
+		std::wcout << "\tfile version:" << fileVersion << std::endl;
+	}
+	else
+	{
+		std::wcout << "file not found: " << strname << std::endl;
+	}
+
+}
+
+void verifyFiles(UiAutoConfig uiautoconfig, IasVersion iasversion, std::string name)
+{
+	std::string arr1[1024];
+	int cnt = uiautoconfig.GetJsonValue(name, arr1);
+	for (int i = 0; i < cnt; i++) {
+		std::string itm = arr1[i];
+		std::wstring widestr = std::wstring(itm.begin(), itm.end());
+		verifyFile(iasversion, widestr.c_str());
+	}
+}
+
+void verifyRegistry(UiAutoConfig uiautoconfig, std::string name, const wchar_t* searchfor)
+{
+	std::string arr1[1024];
+	int cnt = uiautoconfig.GetJsonValue(name, arr1);
+	std::cout << "Printing elements of " << name << std::endl;
+	for (int i = 0; i < cnt; i++) {
+		std::string itm = arr1[i];
+		std::wstring widestr = std::wstring(itm.begin(), itm.end());
+		std::wstring elements[1024];
+		WinReg winreg;
+		HKEY hkey;
+		int sz;
+		long res = winreg.LocalMachineKey(&hkey, widestr.c_str(), elements, &sz);
+		int foundcnt = 0;
+		for (int i = 0; i < sz; i++) {
+			std::wstring s = elements[i];
+			size_t diff=0;
+			diff = wcscmp(s.c_str(), searchfor);
+			if (diff == 0) {
+				foundcnt++;
+			}
+		}
+		if (foundcnt > 0) {
+			std::wcout << "Found product key in registry: " << searchfor << std::endl;
+		}
+		else {
+			std::wcout << "No product key in registry: " << searchfor << std::endl;
+		}
+	}
+
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	UiAutoConfig uiautoconfig(L"LanternRockInstallConfig.json");
-	uiautoconfig.execute("MSIEXEC_INSTALL_WIN32_DEBUG");
-	UiAutoInstallValidate();
+	IasVersion iasversion;
 
-	uiautoconfig.execute("MSIEXEC_INSTALL_WIN32_RELEASE");
-	UiAutoInstallValidate();
-
-	uiautoconfig.execute("MSIEXEC_INSTALL_X64_DEBUG");
-	UiAutoInstallValidate();
-
-	uiautoconfig.execute("MSIEXEC_INSTALL_X64_RELEASE");
-	UiAutoInstallValidate();
+	std::wstring prdid;
+	int cnt = uiautoconfig.GetJsonOneValue("PRODUCT_ID", &prdid);
 
 
-	uiautoconfig.validateFiles("FILES_INSTALL_X86");
-	uiautoconfig.validateFiles("FILES_INSTALL_X64");
+	if (true) {
+		std::wcout << "\n\n==================== WIN32_DEBUG ======================" << std::endl;
+		installValidate(uiautoconfig, "MSIEXEC_INSTALL_WIN32_DEBUG");
+		std::wcout << "Install done." << std::endl;
+		std::wcout << "Verifying dll file and executables:" << std::endl;
+		uiautoconfig.validateFiles("FILES_INSTALL");
+		uiautoconfig.validateFiles("FILES_INSTALL_X86");
+		verifyRegistry(uiautoconfig, "REGISTRY_COMPANY", prdid.c_str());
 
-	uiautoconfig.validateFiles("FILES_INSTALL");
+		verifyFiles(uiautoconfig, iasversion, "FILES_INSTALL");
+		verifyFiles(uiautoconfig, iasversion, "FILES_INSTALL_X86");
 
-	return 0;
+		std::wcout << "Verifying dll file using loadlibrary and getprocaddress:" << std::endl;
+		uiautoconfig.VerifyAddressName("FILES_INSTALL_X86", "DLL_PROC_ADDRESS_NAMES");
 
-	std::wstring elements[1024];
-	WinReg winreg;
-	HKEY hkey;
-	long res = winreg.LocalMachineKey(&hkey, L"SOFTWARE\\www\\ggg 2.0", elements);
-	HKEY hkey2;
-	long res2 = winreg.LocalMachineKey(&hkey2, L"SOFTWARE\\www\\", elements);
+		std::wcout << "Starting uninstall." << std::endl;
+		uninstallValidate(uiautoconfig, "MSIEXEC_UNINSTALL_WIN32_DEBUG");
+		std::wcout << "Uninstall done." << std::endl;
+		std::wcout << "Verifying dll file and executables:" << std::endl;
+		uiautoconfig.validateFiles("FILES_INSTALL");
+		uiautoconfig.validateFiles("FILES_INSTALL_X86");
+		verifyRegistry(uiautoconfig, "REGISTRY_COMPANY", prdid.c_str());
+	}
 
-	HKEY hkey3;
-	wchar_t buf[4096];
-	DWORD dlen = 4096;
-	memset(buf, '\0', 4096);
-	//long res3 = winreg.GetRegValue(&hkey3, L"SOFTWARE\\www\\ggg 2.0", L"Location64", L"", buf, &dlen);
-	long res3 = winreg.GetRegValue(&hkey3, L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", L"SystemRoot", L"", buf, &dlen);
-	//long res3 = winreg.GetRegValue(&hkey3, L"SOFTWARE\\www\\IAS", L"test", L"", buf, &dlen);
-	//long res3 = winreg.GetRegValue(&hkey3, L"SOFTWARE\\www\\Display\\igfxcui", L"Inst", L"", buf, &dlen);
+	if (true) {
+		std::wcout << "\n\n==================== WIN32_RELEASE ======================" << std::endl;
+		installValidate(uiautoconfig, "MSIEXEC_INSTALL_WIN32_RELEASE");
+		std::wcout << "Install done." << std::endl;
+		std::wcout << "Verifying dll file and executables:" << std::endl;
+		uiautoconfig.validateFiles("FILES_INSTALL");
+		uiautoconfig.validateFiles("FILES_INSTALL_X86");
+		verifyRegistry(uiautoconfig, "REGISTRY_COMPANY", prdid.c_str());
 
-	uiautoconfig.execute("MSIEXEC_UNINSTALL_WIN32_DEBUG");
-	UiAutoUninstallValidate();
-	uiautoconfig.execute("MSIEXEC_UNINSTALL_WIN32_RELEASE");
-	UiAutoUninstallValidate();
-	uiautoconfig.execute("MSIEXEC_UNINSTALL_X64_DEBUG");
-	UiAutoUninstallValidate();
-	uiautoconfig.execute("MSIEXEC_UNINSTALL_X64_RELEASE");
-	UiAutoUninstallValidate();
+		verifyFiles(uiautoconfig, iasversion, "FILES_INSTALL");
+		verifyFiles(uiautoconfig, iasversion, "FILES_INSTALL_X86");
 
-	uiautoconfig.validateFiles("FILES_INSTALL_X86");
-	uiautoconfig.validateFiles("FILES_INSTALL_X64");
+		std::wcout << "Verifying dll file using loadlibrary and getprocaddress:" << std::endl;
+		uiautoconfig.VerifyAddressName("FILES_INSTALL_X86", "DLL_PROC_ADDRESS_NAMES");
 
-	uiautoconfig.validateFiles("FILES_INSTALL");
+		std::wcout << "Starting uninstall." << std::endl;
+		uninstallValidate(uiautoconfig, "MSIEXEC_UNINSTALL_WIN32_RELEASE");
+		std::wcout << "Uninstall done." << std::endl;
+		std::wcout << "Verifying dll file and executables:" << std::endl;
+		uiautoconfig.validateFiles("FILES_INSTALL");
+		uiautoconfig.validateFiles("FILES_INSTALL_X86");
+		verifyRegistry(uiautoconfig, "REGISTRY_COMPANY", prdid.c_str());
+	}
 
-	long res4 = winreg.LocalMachineKey(&hkey2, L"SOFTWARE\\www\\", elements);
+	if (false) {
 
+		installValidate(uiautoconfig, "MSIEXEC_INSTALL_WIN32_DEBUG");
+		installValidate(uiautoconfig, "MSIEXEC_INSTALL_WIN32_RELEASE");
+		installValidate(uiautoconfig, "MSIEXEC_INSTALL_X64_DEBUG");
+		installValidate(uiautoconfig, "MSIEXEC_INSTALL_X64_RELEASE");
+
+
+		uiautoconfig.validateFiles("FILES_INSTALL_X86");
+		uiautoconfig.validateFiles("FILES_INSTALL_X64");
+
+		uiautoconfig.validateFiles("FILES_INSTALL");
+
+
+		WinReg winreg;
+		HKEY hkey3;
+		wchar_t buf[4096];
+		DWORD dlen = 4096;
+		memset(buf, '\0', 4096);
+		long res3 = winreg.GetRegValue(&hkey3, L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", L"SystemRoot", L"", buf, &dlen);
+
+		uninstallValidate(uiautoconfig, "MSIEXEC_UNINSTALL_WIN32_DEBUG");
+		uninstallValidate(uiautoconfig, "MSIEXEC_UNINSTALL_WIN32_RELEASE");
+		uninstallValidate(uiautoconfig, "MSIEXEC_UNINSTALL_X64_DEBUG");
+		uninstallValidate(uiautoconfig, "MSIEXEC_UNINSTALL_X64_RELEASE");
+
+		uiautoconfig.validateFiles("FILES_INSTALL_X86");
+		uiautoconfig.validateFiles("FILES_INSTALL_X64");
+
+		uiautoconfig.validateFiles("FILES_INSTALL");
+
+
+	}
 
 	std::cout << "Done" << std::endl;
 	Sleep(3000);
