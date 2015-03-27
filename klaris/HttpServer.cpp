0e46da7623a9f0b479da9eba4d225153c96c5e03
bgonzax <bgonzales20001@gmail.com>
2015-03-27 14:47:02 -0700

    Cleanup a little bit

--- klaris/HttpServer.cpp
+++ klaris/HttpServer.cpp
@@ -1,5 +1,6 @@
 #include "HttpServer.h"
 
+HttpServer::~HttpServer() {}
 
 HttpServer::HttpServer()
 {
@@ -7,82 +8,54 @@ HttpServer::HttpServer()
 	AddRoute("/test", &HttpServer::HandleTest);
 }
 
-
-HttpServer::~HttpServer()
+void HttpServer::EvaluateRoute(HttpHeader *httpHeader)
 {
+	LP_MEM_FUNC lpFunc = m_routes[std::string(httpHeader->GetUrl())];
+	if (lpFunc != NULL) 
+		(this->*lpFunc)();
+	else
+		Handle404();
 }
 
 void HttpServer::HandleTest()
 {
-	printf("Handle Home\n");
-
-	memset(m_SocketInfo->BufferOut, '\0', DATA_BUFSIZE);
 	const char* body = "<html><h1>This is a test page.</h1><br><hr></html>\n";
 	int bodylen = strlen(body);
-
-
 	char content[DATA_BUFSIZE];
+	memset(m_SocketInfo->BufferOut, '\0', DATA_BUFSIZE);
 	memset(content, '\0', DATA_BUFSIZE);
 	sprintf_s(content, DATA_BUFSIZE, "HTTP/1.0 200 OK\nDate: Fri, 31 Dec 1999 23:59:59 GMT\nContent-Type: text/html\nContent-Length: %d\n\n%s", bodylen, body);
-
 	sprintf_s(m_SocketInfo->BufferOut, DATA_BUFSIZE, "%s", content);
 	m_SocketInfo->BytesSEND = strlen(m_SocketInfo->BufferOut);
-
 }
 
-void HttpServer::HandleHome() 
+void HttpServer::HandleHome()
 {
-	printf("Handle Home\n");
-
-	memset(m_SocketInfo->BufferOut, '\0', DATA_BUFSIZE);
 	const char* body = "<html><h1>Klaris web server home page loaded.</h1><br><hr></html>\n";
 	int bodylen = strlen(body);
-
-
 	char content[DATA_BUFSIZE];
+	memset(m_SocketInfo->BufferOut, '\0', DATA_BUFSIZE);
 	memset(content, '\0', DATA_BUFSIZE);
 	sprintf_s(content, DATA_BUFSIZE, "HTTP/1.0 200 OK\nDate: Fri, 31 Dec 1999 23:59:59 GMT\nContent-Type: text/html\nContent-Length: %d\n\n%s", bodylen, body);
-
 	sprintf_s(m_SocketInfo->BufferOut, DATA_BUFSIZE, "%s", content);
 	m_SocketInfo->BytesSEND = strlen(m_SocketInfo->BufferOut);
-
 }
 
 void HttpServer::Handle404()
 {
-	memset(m_SocketInfo->BufferOut, '\0', DATA_BUFSIZE);
 	const char* body = "<html><h1>KLARIS DID NOT FIND IT</h1><br><hr></html>\n";
 	int bodylen = strlen(body);
-
-
 	char content[DATA_BUFSIZE];
+
+	memset(m_SocketInfo->BufferOut, '\0', DATA_BUFSIZE);
 	memset(content, '\0', DATA_BUFSIZE);
 	sprintf_s(content, DATA_BUFSIZE, "HTTP/1.0 404 OK\nDate: Fri, 31 Dec 1999 23:59:59 GMT\nContent-Type: text/html\nContent-Length: %d\n\n%s", bodylen, body);
-
 	sprintf_s(m_SocketInfo->BufferOut, DATA_BUFSIZE, "%s", content);
 	m_SocketInfo->BytesSEND = strlen(m_SocketInfo->BufferOut);
-
 }
 
-void HttpServer::EvaluateRoute(HttpHeader *httpHeader)
+void HttpServer::AddRoute(char *purl, LP_MEM_FUNC lpFunc)
 {
-	//auto x = httpHeader->GetUrl();
-	std::string nm(httpHeader->GetUrl());
-	FUNC lpFunc = m_routes[nm];
-	if (lpFunc != NULL) 
-	{
-		(this->*lpFunc)();
-	}
-	else
-	{
-		Handle404();
-	}
-}
-
-void HttpServer::AddRoute(char *purl, FUNC lpFunc)
-{
-	//pair_t p(purl, lpFunc);
-	//m_routes.push_back(p);
 	m_routes[purl] = lpFunc;
 }
 
@@ -90,9 +63,6 @@ void HttpServer::Request(LPSOCKET_INFORMATION pSocketInfo)
 {
 	printf("Recv:\n%s\n", pSocketInfo->DataBuf.buf);
 	m_SocketInfo = pSocketInfo;
-
 	httpHeader.Parse(pSocketInfo->DataBuf.buf);
 	EvaluateRoute(&httpHeader);
-	//pSocketInfo->BufferOut;
-
 }
--- klaris/HttpServer.h
+++ klaris/HttpServer.h
@@ -11,21 +11,19 @@ public:
 	HttpServer();
 	~HttpServer();
 	
-	typedef std::pair<char*, void*> pair_t;
+	typedef void (HttpServer::*LP_MEM_FUNC) ();
 
 	virtual void Request(LPSOCKET_INFORMATION pSocketInfo);
-	typedef void (HttpServer::*FUNC) ();
-	void AddRoute(char *url, FUNC lpFunc);
+	void AddRoute(char *url, LP_MEM_FUNC lpFunc);
 
 private:
+	std::map <std::string, LP_MEM_FUNC> m_routes;
+	LPSOCKET_INFORMATION m_SocketInfo;
 	HttpHeader httpHeader;
 
-	std::map <std::string, FUNC> m_routes;
 	void HandleHome();
 	void HandleTest();
 	void EvaluateRoute(HttpHeader *httpHeader);
 	void Handle404();
-
-	LPSOCKET_INFORMATION m_SocketInfo;
 };
 
